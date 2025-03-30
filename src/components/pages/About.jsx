7ab2e76d9b4df992b30d6bc5868e952c37da8486
Afjal hussain <mdafjalhussain503@gmail.com>
2025-03-30 18:48:45 +0530

    about & contact pages added

--- src/components/pages/About.jsx
+++ src/components/pages/About.jsx
@@ -1,11 +1,49 @@
 import React from 'react'
+import photo from "../../assets/aboutphot.jpg";
 
 function About() {
   return (
-	<div className='py-15 min-h-[600px]'>
-		fjawdafjl
+	<div className='py-15 min-h-[600px] w-full '>
+		<div className="flex flex-col md:flex-row items-center justify-between p-6 ml-auto md:ml-20 mr-auto md:mr-10">
+     
+     
+		 {/* Left Side*/}
+	  <div className="w-full md:w-1/2 text-center md:text-left  ">
+        <h1 className=" text-4xl md:text-6xl font-bold mb-4"><span className='text-orange-400'>A</span>bout</h1>
+        <p className="text-lg text-center text-justify">
+          This is a paragraph that describes the content of the blog. It's a
+          brief introduction or summary of what the blog is about.
+		  Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta corporis id, esse asperiores voluptatibus ex voluptates ea non veniam? Libero quia harum iure omnis soluta esse dolor minus, minima explicabo, vero mollitia obcaecati ratione voluptates itaque molestias velit eos voluptatem laudantium beatae. Similique eveniet beatae deleniti pariatur vero libero non necessitatibus a qui culpa officiis quisquam sint aperiam dolorem est consequatur cumque quasi rerum, provident asperiores iste, sed debitis officia. Repellendus voluptatem deserunt similique quaerat labore maxime. Facilis ad assumenda ducimus commodi explicabo at, vero beatae. Tenetur officiis, aperiam repudiandae nemo facere blanditiis 
+        </p>
+		<div className=' mt-20 md:mt-20'>
+			<div className='mb-5'>
+			<h1 className='text-4xl md:text-6xl font-bold '>Our <br /><span className='text-orange-400'>M</span>ission</h1>
+			
+			<p className='text-blue-400 text-lg text-center m-5'>Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+				 Ratione, sed! Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis dolor
+				 e nostrum optio praesentium labore laborum? Soluta iste aut magnam ullam?
+			</p>
+			</div>
+
+		</div>
+      </div>
+
+      {/* Right Side */}
+      <div className="w-full md:w-1/2 mt-6 md:mt-0">
+        <img
+          src={photo}  alt=""
+          className="   h-auto object-cover transition-all duration-300 hover:shadow-lg hover:scale-95 mx-auto  rounded-4xl md:h-[500px]  shadow-md shadow-blue-200 hover:shadow-blue-300  "
+        />
+		<p className='text-lg mt-10 md:mt-6 md:text-center text-justify '>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum reprehenderit
+			 numquam veritatis nesciunt, consequuntur molestiae doloribus
+			 accusamus quos, quas earum aut omnis veniam aperiam saepe suscipit neque animi fugit dicta.
+			 
+	    </p>
+      </div>
+	  </div>
+    </div>
 	  
-	</div>
+	
   )
 }
 
--- src/components/pages/ContactPage.jsx
+++ src/components/pages/ContactPage.jsx
@@ -2,11 +2,87 @@ import React from 'react'
 
 function ContactPage() {
   return (
-	<div className='py-15 min-h-[600px]'>
+	<div className='py-15 min-h-[600px] '>
+		<div className='flex flex-col md:flex-row md:px-20  py-10'>
+			{/* left side */}
+			<div className='w-full md:w-1/2 px-4 md:px-0'>
+				<div className='md:mt-15  mt-2'>
+					<h1 className='text-4xl md:text-8xl font-semibold   mb-4'>Contact Us</h1>
+					<p className='text-lg  mb-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo, corrupti.</p>
+					
+					<a href="#rana@mail.com" className='mb-2 text-blue-400 font-bold'>contactme@blog.io</a>
+					<p className=' mb-2 '>321-321-321</p>
+					
+
+				</div>
+
+
+				<div className='flex   flex-col mt-8'>
+				<p className=' mb-2 underline text-blue-400'>Customer Supports</p>
+
+				<div  className='break-words text-justify'>
+
+				<div className='' >
+						<h1  className='text-lg font-medium mb-2'>Customer Supports</h1>
+						<p className='text-lg  mb-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+							maiores expedita dolorem impedit pariatur non enim molestiae voluptas, beatae
+							 nesciunt totam nobis omnis nemo consequatur fuga vero natus aspernatur mollitia?
+						</p>
+					</div>
+					<div className='ml-3' >
+						<h1 className='text-lg font-medium mb-2 '>Feedback and Suggestions</h1>
+						<p className='text-lg mb-2 w-full'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et
+							voluptatum repellat suscipit iste neque in maiores. Dolore ipsa culpa maxime
+							 impedit ex, aspernatur eligendi dignissimos rem totam amet nostrum temporibus?
+						</p>
+					</div>
+					<div className='ml-3'>
+						<h1 className='text-lg font-medium mb-2'>Media Inquiries</h1>
+						<p className='text-lg mb-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Et
+							voluptatum repellat suscipit iste neque in maiores. Dolore ipsa culpa maxime
+							 impedit ex, aspernatur eligendi dignissimos rem totam amet nostrum temporibus?
+						</p>
+					</div>
+
+				</div>
+					
+				</div>
+			</div>
+
+
+
+
+
+
+
+			{/* right side */}
+			<div className='w-full md:w-1/2 mx-auto  '>
+				<div className=' flex justify-center items-center min-h-screen'>
+				<form action="" className='bg-gray-100 p-6 rounded-md shadow-lg w-full max-w-lg capitalize '>
+					<h1  className='text-2xl font-semibold text-center mb-4'>Get In Touch</h1>
+					<p className='text-center mb-6'>submit your problem.</p>
+					<div  className='flex space-x-3 mb-4  flex-col md:flex-row'>
+						<input type="text" placeholder='First name'className='border border-gray-200 rounded-2xl text-center p-3 w-full ' />
+						<input type="text" placeholder='Last name'className='border border-gray-200 rounded-2xl text-center p-3 w-full' />
+					</div>
+					<div className='mb-4'>
+					<input type="email" name="" id="" placeholder='email' className='border border-gray-200 rounded-2xl text-center p-3 w-full' />
+					</div>
+					<div className='mb-4'>
+						<input type="text" name="" id=""  placeholder='mobile number' className='border border-gray-200 rounded-2xl text-center p-3 w-full'/>
+					</div>
+					<div className='mb-4'>
+					<textarea name="text" id="" placeholder='write some things' className='resize-none border border-gray-200 rounded-2xl text-center p-3 w-full' rows={8} ></textarea>
+					</div>
+					<button className='bg-blue-500 text-white py-3 px-6  rounded-2xl w-full hover:bg-blue-600 cursor-pointer'>Submit</button>
+				</form>
+				</div>
+			</div>
+		</div>
 	  
-	  this is the contact page
+	  
 	</div>
   )
 }
 
-export default ContactPage
+export default ContactPage;
